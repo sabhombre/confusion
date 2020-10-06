f46af1eb679e7712c04a79361cf2d7a8bf0981ff
Sabrina Hombrebueno <sabhombrebueno@yahoo.com>
2020-10-06 11:57:43 +0900

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,8 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
     function RenderMenuItem({dish, onClick}){
         return (
-            <Card key={dish.id}
-                onClick={() => onClick(dish.id)}>
+            <Card key={dish.id}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -18,8 +17,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
         const menu = props.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                    onClick={() => props.onClick(dish.id)}>
+                <Card key={dish.id}>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
